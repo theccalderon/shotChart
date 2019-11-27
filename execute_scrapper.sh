7c4844898a7a6c01d1e144a0d833b8a5a5421f49
ccalderon <ccalderon911217@gmail.com>
2019-11-27 11:40:39 -0500

    reference to calendar.json changed so `scrapy crawl basketball-reference -o shots-2000.csv -a season=2000` works from the root dir

--- execute_scrapper.sh
+++ execute_scrapper.sh
@@ -5,7 +5,7 @@ while [ $start_season -le 2019 ]
 do
 #    ../bin/scrapy crawl basketball-reference -o "shots-${start_season}.csv" -a season=${start_season}
 #    touch  "shots-${start_season}.csv"
-    scrapy crawl basketball-reference -o "shots-${start_season}.csv" -a season=${start_season}
+    scrapy crawl basketball-reference -o "./shotChart/data/shots-${start_season}.csv" -a season=${start_season}
     start_season=$((start_season + 1))
 done
 
