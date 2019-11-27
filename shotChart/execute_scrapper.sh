4a37f0f87445ad3f4daffdebff1897bba113ce06
ccalderon <ccalderon911217@gmail.com>
2019-11-27 11:32:39 -0500

    moved execute_scrapper.sh to the root dir

--- shotChart/execute_scrapper.sh
+++ shotChart/execute_scrapper.sh
@@ -3,7 +3,7 @@
 start_season=2000
 while [ $start_season -le 2019 ]
 do
-    ../../../bin/scrapy crawl basketball-reference -o "shots-${start_season}.csv" -a season=${start_season}
+    ../../bin/scrapy crawl basketball-reference -o "shots-${start_season}.csv" -a season=${start_season}
 #    touch  "shots-${start_season}.csv"
     start_season=$((start_season + 1))
 done
