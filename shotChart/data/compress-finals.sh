a38e78311e603e7c596fdf01241e95a7641f3791
Cesar Calderon Muro <cesar.calderon.dev@gmail.com>
2021-04-18 14:50:32 -0400

    2021, April 18th update

--- shotChart/data/compress-finals.sh
+++ shotChart/data/compress-finals.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-start_season=2000
-while [ $start_season -le 2019 ]
+start_season=2019
+while [ $start_season -le 2020 ]
 do
     tar czvf "shots-${start_season}.tgz" "shots-${start_season}-final.csv"
 #    touch  "shots-${start_season}.csv"
